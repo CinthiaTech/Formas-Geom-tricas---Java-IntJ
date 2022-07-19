da413c357ea2be6038a5519539d41fa122887361
Cínthia Eller <cinthiaeller@gmail.com>
2022-07-19 14:04:39 -0300

    Calcular Formas Geométricas - Java IntJ

---
+++ src/calculaFormas.java
@@ -0,0 +1,9 @@
+public class calculaFormas {
+    public static void main(String[] args) {
+        System.out.println("Exercício formas geométricas");
+        formasGeometricas.area(3);
+        formasGeometricas.area(5d,5d);
+        formasGeometricas.area(7,8,9);
+        formasGeometricas.area(5f,5f);
+    }
+}
